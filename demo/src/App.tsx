83f04f6b2a5a4e56601c6286c368faee122e5c8d
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-06-08 10:55:46 +0430

    add demo

---
+++ demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {} from 'kuknos-browser-intent-sdk'
+
+function App() {
+
+  return (
+    <div className="App">
+      kuknos browser intent sdk
+    </div>
+  );
+}
+
+export default App;
