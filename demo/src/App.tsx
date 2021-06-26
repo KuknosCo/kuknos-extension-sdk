5c9be9f470837c6ddb51882ea8f030278098103c
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-06-26 11:34:17 +0430

    update README

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,26 +1,9 @@
-import React , {useState , useEffect} from 'react';
-import KuknosIntent from 'kuknos-browser-intent-sdk'
+import KuknosIntent from 'kuknos-extension-sdk'
 function App() { 
 
   const submit = async ()=>{
     
-    /* console.log(btoa(JSON.stringify({
-      data : 'matin',
-      type : 'warn',
-      location : 'login',
-      userAgent: navigator.userAgent
-    })));
-     */
-
-    KuknosIntent.setExtensionUrl('http://localhost:3000')
-
-
-    KuknosIntent.payment({
-      amount: 0.0001,
-      destination: 'GARTVC5KJUEBPXSNDKFXBPK7U5QZJCBEJMWQ66HE7AHFSDYWB6TG5N6C',
-      asset_code: 'PMN',
-      memo: 'matin'
-    })
+    KuknosIntent.getAccountPublicKey()
     .then((result) => {
       console.log(result);
       
@@ -30,20 +13,10 @@ function App() {
     });
 
   }
-
-/*   const [test , setTest] = useState('http://localhost:3000');
-  useEffect(()=>{
-    KuknosIntent.setExtensionUrl(test)
-    KuknosIntent.setNetwork('test')
-  },[test])
- */
-
   
   return (
     <div className="App"> 
-      <button onClick={()=>{submit()}}>kuknos browser intent sdk</button>
-      <button onClick={()=>{KuknosIntent.setNetwork('test')}}>test</button>
-      <button onClick={()=>{KuknosIntent.setNetwork('public')}}>public</button>
+      <button onClick={()=>{submit()}}>kuknos-extension-sdk</button>
     </div>
   );
 }
